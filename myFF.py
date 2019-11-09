0f6ecfdb740ba3f2d748c72f763bddce18994713
Zining Fan <zf2234@columbia.edu>
2019-11-08 20:34:37 -0500

    11.8

--- myFF.py
+++ myFF.py
@@ -1,4 +1,6 @@
 import networkx as nx
+import matplotlib.pyplot as plt
+
 G = nx.Graph()
 
 usa = open('contiguous-usa.dat')
@@ -44,14 +46,22 @@ def flow_with_demands(graph):
             flow_dict[u].update(d)
         return(flow_dict)
     
-    def ComputeGraphR(graphNew,graphR,flow_dict):
+    def ComputeGraphR(graphNew,flow_dict):
+        graphR=graphNew.copy()
         for u,v in graphNew.edges():
-            if flow_dict[u][v]<graphNew.edges[u,v]['capacity']:
+            if flow_dict[u][v] < graphNew.edges[u,v]['capacity']:
+                graphR.add_edge(u,v)
                 graphR.edges[u,v]['capacity']=graphNew.edges[u,v]['capacity']-flow_dict[u][v]
                 if graphR.edges[u,v]['capacity']==0:
                     graphR.remove_edge(u,v)
             if flow_dict[u][v]>0:
+                graphR.add_edge(v,u)
                 graphR.edges[v,u]['capacity']=flow_dict[u][v]
+            if flow_dict[u][v]==graphNew.edges[u,v]['capacity']:
+                graphR.remove_edge(u,v)
+
+        # nx.draw(graphR)
+        # plt.show()
         return(graphR)
 
     def bfs(graphR):
@@ -73,7 +83,7 @@ def flow_with_demands(graph):
     def AugmentPath(graphNew,graphR,path,flow_dict):
         bottleneck=min(graphR.edges[u,v]['capacity'] for (u,v) in path)
         for u,v in path:
-            if (u,v) in graphR.edges():
+            if (u,v) in graphNew.edges():
                 flow_dict[u][v]=flow_dict[u][v]+bottleneck
             elif (v,u) in graphNew.edges():
                 flow_dict[v][u]=flow_dict[v][u]-bottleneck
@@ -95,11 +105,15 @@ def flow_with_demands(graph):
         if d < 0:
             graphNew.add_edge('s',state)
             graphNew.edges['s',state]['capacity']=-d
+            # graphNew.add_edge(state,'s')
+            # graphNew.edges[state,'s']['capacity']=-d
             # compute the sum of demands 
             f=f-d
         if d > 0:
             graphNew.add_edge(state,'t')
             graphNew.edges[state,'t']['capacity']=d
+            # graphNew.add_edge('t',state)
+            # graphNew.edges['t',state]['capacity']=d
     
     flow_dict=InitializeFlow_dict()
     graphR=graphNew.copy()
@@ -107,7 +121,7 @@ def flow_with_demands(graph):
 
     while len(path)>0:
         flow_dict=AugmentPath(graphNew,graphR,path,flow_dict)
-        graphR=ComputeGraphR(graphNew,graphR,flow_dict)
+        graphR=ComputeGraphR(graphNew,flow_dict)
         path=bfs(graphR)
     a=0
     
